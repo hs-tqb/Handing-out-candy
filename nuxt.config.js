66c84e134c74698aeb9a181e8a8ab480d69ef054
hs <huangshuai@utianqi.com>
2018-03-28 11:57:39 +0800

    html 头部声明，不允许用户缩放页面

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'Handing out candy',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
